91aad81731d85d1fefdee071b4b303dc00386796
Armando De La Rosa <37047175+ArmandoDLaRosa@users.noreply.github.com>
2022-09-18 16:18:15 -0500

    Etl and utils updates

--- ETL/ETL.py
+++ ETL/ETL.py
@@ -4,15 +4,16 @@ import pandas as pd
 def extraction(path:str)  -> pd.DataFrame:
     return pd.read_csv(path)
 
-def transform(df:pd.DataFrame) -> pd.DataFrame:
-    # Feature Selection
-    df = df.drop(columns=utils.Maps.survival_predictors)
+def transform(df:pd.DataFrame, kaggle:bool = False) -> pd.DataFrame:
 
-    # Feature Engineering
-        # Jobs - Mrs, Miss ************* MISsING considerarlo en la age
-        # Sib Ranges
-        # Age Ranges
-    
+    # Feature Engineering ---- Jobs 
+    df = utils.title_engineering(df)        
+    #  Feature Engineering ---- Sib Ranges
+    #  Feature Engineering ---- Age Ranges
+
+    # Feature Selection
+    df = df.drop(columns=utils.Maps.kaggle_predictors) if kaggle else df.drop(columns=utils.Maps.survival_predictors)
+
     # Data Enriching
 
     # Encoding
@@ -27,8 +28,8 @@ def load(df:pd.DataFrame, path:str) -> pd.DataFrame:
     df.to_csv(path)
     return df
 
-def etl(extract_path:str="Titanic/train.csv", load_path:str="./output", return_df:bool = True) -> pd.DataFrame:
-    return load(transform(extraction(extract_path)), load_path) if return_df else "dataset output -done"
+def etl(extract_path:str="Titanic/train.csv", load_path:str="./output", return_df:bool = True, kaggle:bool = False) -> pd.DataFrame:
+    return load(transform(extraction(extract_path), kaggle), load_path) if return_df else "dataset output -done"
 
 if __name__ == '__main__':
     etl()
--- ETL/utils.py
+++ ETL/utils.py
@@ -2,6 +2,22 @@ import pandas as pd
 import pickle
 pd.options.mode.chained_assignment = None
 
+class MLmodel:
+    def __init__(self, name="", accuracy=0, precision=0, recall=0, f1=0):
+        self.name = name
+        self.accuracy = accuracy
+        self.precision = precision
+        self.recall = recall
+        self.f1 = f1
+    def to_dict(self):
+        return {
+            'name': self.name,
+            'accuracy': self.accuracy,
+            'precision': self.precision,
+            'recall': self.recall,
+            'f1': self.f1     
+        }
+        
 class Maps:
     sex = {
         "male"  :   0,
@@ -12,18 +28,42 @@ class Maps:
         "Q" :   1,
         "S" :   2
     }
+    title = {
+        # Royal - 0
+        "the Countess" : 0,
+        "Jonkheer" : 0,
+        "Lady" : 0,
+        # Normal - 1
+        "Don" : 1,
+        "Dona" : 1,
+        "Miss" : 1,
+        "Ms" : 1,
+        "Mlle" : 1,
+        "Mrs" : 1,
+        "Mr" : 1,
+        "Sir" : 1,
+        "Master" : 1, # Kid
+        # Profession - 2
+        "Rev" : 2,
+        "Capt" : 2,
+        "Col" : 2,
+        "Dr" : 2,
+        "Major" : 2
+    }  
+
     var_types =  {
         "number"    :   "median",
         "object"    :   "mode"
     }
-    age_predictors = ["Pclass", "Sex", "Fare"]
-    survival_predictors = ['PassengerId', 'Name', 'SibSp', 'Ticket', 'Cabin']
-    
+    age_predictors = ["Title", "Sex", "Pclass"] # This isn't used to drop columns
+    survival_predictors = ['PassengerId','SibSp', 'Ticket', 'Cabin', 'Parch', 'Fare', 'Name']  # This is used to drops columns
+    kaggle_predictors = ['SibSp', 'Ticket', 'Cabin', 'Parch', 'Fare', 'Name']  # This is used to drops columns
+
 def age_imputing(df:pd.DataFrame) -> pd.DataFrame:
     with open('./age_model' , 'rb') as f:
         model = pickle.load(f)
     df_null = df[df["Age"].isnull()]
-    not_null =  df[~df["Age"].isnull()]
+    not_null =  df[(~df["Age"].isnull()) |  (df["Age"] <= 0)]
     df_null["Age"] = model.predict(df_null[Maps.age_predictors])
     df_null["Age"] = df_null["Age"].round()
     return pd.concat([not_null, df_null])
@@ -37,4 +77,9 @@ def imputing(df:pd.DataFrame) -> pd.DataFrame:
 def encoding(df:pd.DataFrame) -> pd.DataFrame:
     df['Sex'] = df['Sex'].map(Maps.sex)
     df['Embarked'] = df['Embarked'].map(Maps.embarked)
+    df['Title'] = df['Title'].map(Maps.title)
+    return df
+
+def title_engineering(df: pd.DataFrame) -> pd.DataFrame:
+    df["Title"]=[i.split(".")[0].split(",")[-1].strip() for i in df["Name"]]
     return df
